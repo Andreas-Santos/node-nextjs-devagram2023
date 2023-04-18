6d4bd4c163c41f1bd621de44184e26507ee213f8
Andreas Santos <andreassantos.3003@gmail.com>
2023-04-17 21:07:26 -0300

    integração cosmic e multer

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -4,39 +4,49 @@ import type {CadastroRequisicao} from '../../types/CadastroRequisicao';
 import {UsuarioModel} from '../../models/UsuarioModel';
 import { conectarMongoDB } from '@/middlewares/conectarMongoDB';
 import md5 from 'md5';
+import {upload, uploadImagemCosmic} from '../../services/uploadImagemCosmic';
+import nc from 'next-connect';
 
-const endpointCadastro = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+const handler = nc()
+    .use(upload.single('file'))
+    .post(async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
 
-    if(req.method === 'POST'){
         const usuario = req.body as CadastroRequisicao;
-
+    
         if(!usuario.nome || usuario.nome.length < 2){
             return res.status(400).json({erro : 'Nome inválido'});
         }
-
+    
         if(!usuario.email || usuario.email.length < 5 || !usuario.email.includes('@') || !usuario.email.includes('.')){
             return res.status(400).json({erro : 'Email inválido'});
         }
-
+    
         if(!usuario.senha || usuario.senha.length < 4){
             return res.status(400).json({erro : 'Senha inválida'});
         }
-
+    
         const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
         if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
             return res.status(400).json({erro : 'Este email já possuí um cadastro'});
         }
+    
+        const image = await uploadImagemCosmic(req);
 
         const usuarioASerSalvo = {
             nome: usuario.nome,
             email: usuario.email,
-            senha: md5(usuario.senha)
+            senha: md5(usuario.senha),
+            avatar: image?.file?.url
         }
-        
+            
         await UsuarioModel.create(usuarioASerSalvo);
         return res.status(200).json({msg : 'Usuário criado com sucesso'});
+    });
+
+export const config = {
+    api : {
+        bodyParser : false
     }
-    return res.status(405).json({erro : 'Método informado não é válido'});
 }
 
-export default conectarMongoDB(endpointCadastro);
+export default conectarMongoDB(handler);
