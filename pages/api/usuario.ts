d0b61e3147983c12dc5800417001ad7528e13ce3
Andreas Santos <andreassantos.3003@gmail.com>
2023-04-18 17:07:43 -0300

    feed de usuário logado - endpoint

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,9 +1,21 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
-import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
+import {UsuarioModel} from '@/models/UsuarioModel';
+import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 
-const endpointUsuario = (req : NextApiRequest, res : NextApiResponse) => {
+const endpointUsuario = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) => {
 
-    return res.status(200).json({msg : 'Usuário autenticado com sucesso'});
+    try{
+        const {userId} = req?.query;
+        const usuario = await UsuarioModel.findById(userId);
+        usuario.senha = null;
+        return res.status(200).json(usuario);
+    }catch(e){
+        console.log(e);
+    }
+
+    return res.status(400).json({erro : 'Não foi possível obter dados do usuário'});
 }
 
-export default validarTokenJWT(endpointUsuario);
+export default validarTokenJWT(conectarMongoDB(endpointUsuario));
