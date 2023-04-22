a7b48fd9919b24f11dca7b68dd22ecb515b41cc5
Andreas Santos <andreassantos.3003@gmail.com>
2023-04-21 22:20:17 -0300

    Criação do endpoint de Pesquisa

---
+++ pages/api/pesquisa.ts
@@ -0,0 +1,30 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+import { validarTokenJWT } from '@/middlewares/validarTokenJWT';
+import { conectarMongoDB } from '@/middlewares/conectarMongoDB';
+import { UsuarioModel } from '@/models/UsuarioModel';
+
+const endpointPesquisa = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any[]>) => {
+
+    try{    
+        if(req.method === 'GET'){
+            const {filtro} = req.query;
+            if(!filtro || filtro.length < 2){
+                return res.status(400).json({erro : 'Favor informar ao menos 2 caracteres para efetuar a pesquisa'});
+            }
+
+            const usuariosEncontrados = await UsuarioModel.find({
+                nome : {$regex : filtro, $options : 'i'}
+            });
+
+            return res.status(200).json(usuariosEncontrados);
+        }
+
+        return res.status(405).json({erro : 'O método informado não é válido'});
+    }catch(e){
+        console.log(e);
+        return res.status(400).json({erro : 'Erro ao pesquisar usuário'});
+    }
+}
+
+export default validarTokenJWT(conectarMongoDB(endpointPesquisa));
