d4beab7a40beca5dfd2df5d3ef280f02a1c07af5
Andreas Santos <andreassantos.3003@gmail.com>
2023-04-22 19:15:26 -0300

    Feed principal - Home

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -4,6 +4,7 @@ import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
 import {UsuarioModel} from '@/models/UsuarioModel';
 import {PublicacaoModel} from '@/models/PublicacaoModel';
+import { SeguidorModel } from '@/models/SeguidorModel';
 
 const endpointFeed = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) => {
     try{
@@ -19,8 +20,39 @@ const endpointFeed = async (req : NextApiRequest, res : NextApiResponse<Resposta
                     .sort({data : -1});
 
                 return res.status(200).json(publicacoes);
+            }else{
+                const {userId} = req?.query;
+                const usuarioLogado = await UsuarioModel.findById(userId);
+                if(!usuarioLogado){
+                    return res.status(400).json({erro : 'Usuário logado não encontrado'});
+                }
+
+                const seguidores = await SeguidorModel.find({usuarioId : usuarioLogado._id});
+                const seguidoresIds = seguidores.map(s => s.usuarioSeguidoId);
+
+                const publicacoes = await PublicacaoModel
+                    .find({
+                        $or : [
+                            {idUsuario : usuarioLogado._id},
+                            {idUsuario : seguidoresIds}
+                        ]
+                    })
+                    .sort({data : -1});
+
+                    const result = [];
+                    for(const publicacao of publicacoes){
+                        const usuarioPublicacao = await UsuarioModel.findById(publicacao.idUsuario);
+                        if(usuarioPublicacao){
+                            const final = {...publicacao._doc, usuario : {
+                                nome : usuarioPublicacao.nome,
+                                avatar : usuarioPublicacao.avatar
+                            }};
+                            result.push(final);
+                        }
+                    }
+
+                return res.status(200).json(result);
             }
-            return res.status(400).json({erro : 'Parâmetros de pesquisa não informados'});
         }
 
         return res.status(405).json({erro : 'O método informado não é válido'});
